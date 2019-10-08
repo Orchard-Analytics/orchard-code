49947ccbec9e4d86ff5f9479083fba2c420ef33d
rolo93 <rccassirer@gmail.com>
2019-10-08 16:57:31 -0400

    Update funnel_logic.sql

--- funnel_logic.sql
+++ funnel_logic.sql
@@ -1,5 +1,5 @@
-The following is code for creating a lead funnel in Looker using a common leads infastructure. The logic can be easily modified
-for other BI tools. 
+--The following is code for creating a lead funnel in Looker using a common leads infastructure. The logic can be easily modified
+--for other BI tools. 
 
 Suppose your lead stage table contains the following: 
 Lead ID | Stage    | Stage_Updated_Timestamp 
@@ -12,8 +12,8 @@ Lead ID | Stage    | Stage_Updated_Timestamp
 
 
 
-FIRST -- create a new table with every lead, and the first time it moved through a given stage. It's important to use the min 
-function to avoid double counting. 
+--FIRST -- create a new table with every lead, and the first time it moved through a given stage. It's important to use the min 
+--function to avoid double counting. 
 
 SELECT 
   lead_id, 
@@ -25,7 +25,7 @@ SELECT
 FROM lead_stage_table 
 
 
-SECOND -- implement the "is following" logic that is key to funnels. Here is an easy way to think about it: 
+--SECOND -- implement the "is following" logic that is key to funnels. Here is an easy way to think about it: 
 Stage  | Lead Count  | Explanation 
  New   |    100      | 100 leads are new or "following" in the stages Qlfd, Apprv, Active, Rjctd
 Qlfd   |    80       | Between New and Qlfd only 80 leads make it to Qlfd or "following" in the stages Apprv, Active, Rjctd
@@ -34,7 +34,7 @@ Active |    50       | Between Apprv and Active only 50 leads make it to Active 
 Rjctd  |    50       | Simply the number of leads that have been rejected 
 
 
-In Looker, the implementation of the logic is just to create a few measures in the format: 
+--In Looker, the implementation of the logic is just to create a few measures in the format: 
   
     dimension: lead_id {
     description: "Lead ID"
@@ -68,7 +68,7 @@ In Looker, the implementation of the logic is just to create a few measures in t
   }
  
  
- THIRD -- Create a measure for each stage and you'll have your funnel! 
+ --THIRD -- Create a measure for each stage and you'll have your funnel! 
  
  
  
